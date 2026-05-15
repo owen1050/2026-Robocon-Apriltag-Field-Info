5bfa03dc8f18ce44f000c9930e4846b3c86a7885
Owen Busler <owen1050@gmail.com>
2026-05-15 09:43:41 -0400

    code update, new image, and beginings of as layout

--- 2026RoboconTestWPIProject/src/main/java/frc/robot/Robot.java
+++ 2026RoboconTestWPIProject/src/main/java/frc/robot/Robot.java
@@ -4,59 +4,141 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimesliceRobot;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimesliceRobot documentation. If you change the name of this class or the package after
- * creating this project, you must also update the Main.java file in the project.
- */
-public class Robot extends TimesliceRobot {
-  /** Robot constructor. */
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.trajectory.PathPlannerTrajectory;
+import com.pathplanner.lib.trajectory.PathPlannerTrajectoryState;
+import com.pathplanner.lib.util.FileVersionException;
+import com.pathplanner.lib.util.FlippingUtil;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+
   public Robot() {
-    // Run robot periodic() functions for 5 ms, and run controllers every 10 ms
-    super(0.005, 0.01);
 
-    // LiveWindow causes drastic overruns in robot periodic functions that will
-    // interfere with controllers
-    LiveWindow.disableAllTelemetry();
+    // this line updates path planner to know the size of the field so when you call
+    // flipPath it flips along the robocon midfield rather than the default midfield
+    FlippingUtil.fieldSizeX = 15.68;
+    testFlippingPath();
+    importRoboConAprilTagLayout();
 
-    // Runs for 2 ms after robot periodic functions
-    schedule(() -> {}, 0.002);
+  }
 
-    // Runs for 2 ms after first controller function
-    schedule(() -> {}, 0.002);
+  //this imports a path and flips it. both paths are converted to trajectories and put on SD to be viewed on advantage scope to verify the flip worked on the robocon field
+  public void testFlippingPath() {
+    Field2d originaField2d = new Field2d();
+    Field2d mirrorField2d = new Field2d();
 
-    // Total usage:
-    // 5 ms (robot) + 2 ms (controller 1) + 2 ms (controller 2) = 9 ms
-    // 9 ms / 10 ms -> 90% allocated
+    try {
+      PathPlannerPath path = PathPlannerPath.fromPathFile("Test");
+      PathPlannerTrajectory originalTraj = path.generateTrajectory(new ChassisSpeeds(), new Pose2d().getRotation(),
+          RobotConfig.fromGUISettings());
+      originaField2d.getObject("traj").setTrajectory(ppTrajToWPITraj(originalTraj));
+
+      PathPlannerPath mirroredPath = path.flipPath();
+      PathPlannerTrajectory mirTraj = mirroredPath.generateTrajectory(new ChassisSpeeds(), new Pose2d().getRotation(),
+          RobotConfig.fromGUISettings());
+      mirrorField2d.getObject("traj").setTrajectory(ppTrajToWPITraj(mirTraj));
+
+      SmartDashboard.putData("Original", originaField2d);
+      SmartDashboard.putData("Mirrored", mirrorField2d);
+
+    } catch (FileVersionException | IOException | ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  //this imports the april tag layout for robocon and then puts all the poses of april tags on the dashboard to verify it matches new field map
+  public void importRoboConAprilTagLayout() {
+    String path = Filesystem.getDeployDirectory().getAbsolutePath() + "\\2026-robocon-welded-photonvision-wpilib.json";
+    try {
+      AprilTagFieldLayout atflRobocon = new AprilTagFieldLayout(path);
+                                                                      
+      for (int i = 1; i <= 50; i++) {
+        try {
+          Field2d aprilTag = new Field2d();
+          aprilTag.setRobotPose(atflRobocon.getTagPose(i).get().toPose2d());
+          SmartDashboard.putData("Tag" + Integer.toString(i), aprilTag);
+        } catch (Exception e) {
+          continue;
+        }
+        
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+
+  public Trajectory ppTrajToWPITraj(PathPlannerTrajectory traj) {
+    List<PathPlannerTrajectoryState> stateList = traj.getStates();
+    List<Trajectory.State> wpiStateLists = new ArrayList<Trajectory.State>();
+    for (PathPlannerTrajectoryState state : stateList) {
+      Trajectory.State thisWPIState = new Trajectory.State(state.timeSeconds,
+          state.linearVelocity,
+          0,
+          state.pose,
+          0);
+      wpiStateLists.add(thisWPIState);
+    }
+    return new Trajectory(wpiStateLists);
+  }
 }
